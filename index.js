58e3a68afb086648730aec341be294c43b43c51e
Mugambi <javan.mugambi@student.moringaschool.com>
2022-11-28 17:36:20 +0300

    test done

--- index.js
+++ index.js
@@ -1,48 +1,110 @@
-function netSalary() {
-	let salary = document.getElementById("salary").value;
-	
-	// calculating the net salary below 0. 
-    
-	if (salary < 0){
-	
-	    document.getElementById("net").innerHTML = `Net salary is 0`
+function grossIncome(basicSalary, ...allowances){
+	let sum = basicSalary
+	for(let allowance of allowances){
+	   sum += allowance
 	}
-       
-	 // calculating the net salary 24,000 using  NHIF rate of 2.5% and NSSF rate of 6%.
-    
-	else if (salary < 24000){
-	    let tax = salary * 0.1;
-	    let nssf = salary * 0.06;
-	    let nhif = salary * 0.025;
-	    let finSalary = salary - (tax + nssf + nhif);
-    
-	    document.getElementById("net").innerHTML = `Net salary is: ${finSalary}`
+	return sum
 	}
-    
-	 // calculating the net salary above 24,000 and below 32333 using  NHIF rate of 2.5% and NSSF rate of 6%. 
-    
-	else if (salary >= 24000 && salary < 32333 ){
-	    let tax = salary * 0.25;
-	    let nssf = salary * 0.06;
-	    let nhif = salary * 0.025;
-	    let finSalary = salary - (tax + nssf + nhif);
-    
-	    document.getElementById("net").innerHTML = `Net salary is: ${finSalary}`
-	}
-    
-	 // calculating the net salary above 32333 using  NHIF rate of 2.5% and NSSF rate of 6%.
-    
-	else if (salary > 32333){
-	    let tax = salary * 0.3;
-	    let nssf = salary * 0.06;
-	    let nhif = salary * 0.025;
-	    let finSalary = salary - (tax + nssf + nhif);
-    
-	    document.getElementById("net").innerHTML = `Net salary is: ${finSalary}`
-	}
-    
-	else {
-	    document.getElementById("net").innerHTML = `Enter salary`
-	}
-	
-    }
+   
+	//console.log(grossIncome(20000, 6000, 43000))
+	function NHIF(grossPay){
+	   if(grossPay < 6000){
+	       return 150
+	   } else if (grossPay < 6000){
+	       return 300
+	   }else if(grossPay < 12000){
+	       return 400
+	   }
+	   else if(grossPay < 15000){
+	       return 500
+	   }
+	   else if(grossPay < 20000){
+	       return 600
+	   }
+	   else if(grossPay < 25000){
+	       return 750
+	   }
+	   else if(grossPay < 30000){
+	       return 850
+	   }
+	   else if(grossPay < 35000){
+	       return 900
+	   }
+	   else if(grossPay < 40000){
+	       return 950
+	   }
+	   else if(grossPay < 45000){
+	       return 1000
+	   }
+	   else if(grossPay < 50000){
+	       return 1100
+	   }
+	   else if(grossPay < 60000){
+	       return 1200
+	   }
+	   else if(grossPay < 70000){
+	       return 1300
+	   }
+	   else if(grossPay < 80000){
+	       return 1400
+	   }
+	   else if(grossPay < 90000){
+	       return 1500
+	   }
+	   else if(grossPay < 100000){
+	       return 1600
+	   }
+	   else if(grossPay >= 100000){
+	       return 1700
+	   }
+       }
+       //console.log(calculateNHIF(60000))
+   
+       function NSSF(pensionablePay){
+	   return pensionablePay * 0.06
+       }
+       //console.log(NSSF(7000))
+   
+       function taxablePay(grossIncome, ...deductions){
+	   for(let deduction of deductions){
+	       grossIncome -=deduction
+	   }
+	   return grossIncome
+       }
+       //console.log(taxablePay(50000, 1100, 5000))
+   
+       function PAYEE(taxablepay){
+	   if(taxablepay <= 24000){
+	       return taxablepay * 0.1
+	   } else if (taxablepay <= 32333){
+	       return taxablepay * 0.25
+	   } else if(taxablepay > 32333){
+	       return taxablepay * 0.3
+	   }
+       }
+       //console.log(PAYEE(160000))
+   
+       function netPay(taxablepay, payee){
+	   return taxablepay - payee
+       }
+       //console.log(netPay(100000, 20000))
+       function handleSalary(event){
+	   let val = parseInt(document.getElementById("Basic").value)
+	   let basicSalary= Boolean(val)? val : 0
+       //console.log(basicSalary)
+       let allowances = Array.from(document.getElementsByClassName("allowances")).map(function(element){
+	   return Boolean(element.value) ? parseInt(element.value) : 0
+       })
+       //console.log(allowances)
+       let gross = grossIncome(basicSalary, ...allowances)
+       let nhifdeductions = NHIF(gross)
+       let nssfdeductions = NSSF(gross)
+       let taxableincome = taxablePay(gross, nhifdeductions, nssfdeductions)
+       let payee = PAYEE(taxableincome)
+       let net =netPay(taxableincome, payee)
+       document.getElementById("nhif").textContent = nhifdeductions
+       document.getElementById("nssf").textContent = nssfdeductions
+       document.getElementById("payee").textContent = payee
+       document.getElementById("taxableincome").textContent = taxableincome
+       document.getElementById("netpay").textContent = net
+       }
